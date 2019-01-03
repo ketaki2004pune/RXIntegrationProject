d37dcfa3aefa8c332d763f79cd6359d02734a44e
ketaki2004pune <42198235+ketaki2004pune@users.noreply.github.com>
2019-01-03 19:32:27 +0530

    Update file_copy.bat

--- file_copy.bat
+++ file_copy.bat
@@ -2,7 +2,7 @@
 cd C:\Users\Administrator\bamboo-agent-home\xml-data\build-dir\EX-PLANTEST-TES
 del /s *.xml
 set source=C:\Automation\Login_Logout\Execute_Suit\Output
-set dest=C:\Users\Administrator\bamboo-agent-home\xml-data\build-dir\EX-PLANTEST-TES
+set dest=C:\Users\Administrator\bamboo-agent-home\xml-data\build-dir
 pushd "%source%"
 for /f "tokens=*" %%G in ('dir *.xml /b /a-d /od') do SET newest=%%G
 
